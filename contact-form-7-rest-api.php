0ce7ab08e5aebd8a0ef2e9c883de48c5744f65c5
Brad Tollett <tollett.bradley@gmail.com>
2017-04-30 21:23:18 -0500

    Bug Fixes, Generated PHP Docs & Added TODO's

--- contact-form-7-rest-api.php
+++ contact-form-7-rest-api.php
@@ -1,34 +1,33 @@
 <?php
 
 /**
- * Plugin Name: Contact Form 7 REST API Endpoints
- * Description: Contact Form 7 endpoints for the WP REST API v2
- * Version: 0.1
+ * Plugin Name: Contact Form 7 Rest API
+ * Description: Adds Contact Form 7 get, create & update endpoints to the WP REST API v2
+ * Version: 1.0
  * Author: Bradley Tollett
  * Plugin URI: https://github.com/CodeBradley/contact-form-7-rest-api
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	die( "You can't do anything by accessing this file directly." );
+    die( "You can't do anything by accessing this file directly." );
 }
 
 if ( !function_exists( 'is_plugin_active' ) ) {
-	require_once(ABSPATH . '/wp-admin/includes/plugin.php');
+    require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
 }
 
-/* Adds our callback function to the rest_api_init hook if Contact Form 7 is active so that it's loaded with the REST API */
 if ( is_plugin_active('contact-form-7/wp-contact-form-7.php') ) {
-	add_action('rest_api_init', 'register_rest_routes');
+    /* Adds our callback function to the rest_api_init hook if Contact Form 7 is active so that it's loaded with the REST API */
+    add_action('rest_api_init', 'register_rest_routes');
 }
 
-/* Our callback loads and registers the custom endpoint class */
 function register_rest_routes() {
 
-	if(!class_exists('WP_REST_Contact_Form_7_Controller')) {
-		require_once plugin_dir_path(__FILE__).'/lib/endpoints/class-wp-rest-contact-form-7-controller.php';
-	}
+    if(!class_exists('WP_REST_Contact_Form_7_Controller')) {
+        require_once plugin_dir_path(__FILE__).'/lib/endpoints/class-wp-rest-contact-form-7-controller.php';
+    }
 
-	$controller = new WP_REST_Contact_Form_7_Controller();
-	$controller->register_routes();
+    $controller = new WP_REST_Contact_Form_7_Controller();
+    $controller->register_routes();
 
 }
--- lib/endpoints/class-wp-rest-contact-form-7-controller.php
+++ lib/endpoints/class-wp-rest-contact-form-7-controller.php
@@ -9,12 +9,29 @@ if(!class_exists('WPCF7_ContactForm')) {
     require_once( WPCF7_PLUGIN_URL . '/includes/contact-form.php' );
 }
 
+/**
+ * Class WP_REST_Contact_Form_7_Controller
+ * TODO: The keys for the properties in prepare_item_for_response are not going to match the schema that I declared, this needs to be fixed.
+ * TODO: The schema needs to be reworked completely in this. Try and mimick the Post_Controller schema in the default Rest API Endpoints.
+ */
 class WP_REST_Contact_Form_7_Controller extends WP_REST_Controller {
 
+    /**
+     * @var string
+     */
     protected $rest_base = "forms";
+    /**
+     * @var string
+     */
     protected $namespace = "wpcf7/v1";
+    /**
+     * @var string
+     */
     protected $post_type = WPCF7_ContactForm::post_type;
 
+    /**
+     *
+     */
     public function register_routes() {
         register_rest_route( $this->namespace, '/' . $this->rest_base, array(
             array(
@@ -48,10 +65,18 @@ class WP_REST_Contact_Form_7_Controller extends WP_REST_Controller {
         ) );
     }
 
-    public function get_items_permissions_check( $request ) {
+    /**
+     * @param WP_REST_Request $request
+     * @return bool|WP_Error
+     */
+    public function get_items_permissions_check($request ) {
         return $this->get_item_permissions_check($request);
     }
 
+    /**
+     * @param WP_REST_Request $request
+     * @return mixed|WP_REST_Response
+     */
     public function get_items($request) {
         $args = array();
         $query = WPCF7_ContactForm::find($args);
@@ -66,7 +91,11 @@ class WP_REST_Contact_Form_7_Controller extends WP_REST_Controller {
         return $response;
     }
 
-    public function get_item_permissions_check( $request ) {
+    /**
+     * @param WP_REST_Request $request
+     * @return bool|WP_Error
+     */
+    public function get_item_permissions_check($request ) {
         $post_type = get_post_type_object( $this->post_type );
         if (! current_user_can( $post_type->cap->edit_others_posts ) ) {
             return new WP_Error( 'rest_forbidden_context', __( 'Sorry, you are not allowed to view posts of this post type' ), array( 'status' => rest_authorization_required_code() ) );
@@ -74,6 +103,10 @@ class WP_REST_Contact_Form_7_Controller extends WP_REST_Controller {
         return true;
     }
 
+    /**
+     * @param WP_REST_Request $request
+     * @return mixed|WP_Error|WP_REST_Response
+     */
     public function get_item($request) {
         $id = (int) $request['id'];
         $form = WPCF7_ContactForm::get_instance( $id );
@@ -88,6 +121,10 @@ class WP_REST_Contact_Form_7_Controller extends WP_REST_Controller {
         return $response;
     }
 
+    /**
+     * @param WP_REST_Request $request
+     * @return bool|WP_Error
+     */
     public function create_item_permissions_check($request) {
         $post_type = get_post_type_object( $this->post_type );
         if (! current_user_can( $post_type->cap->publish_posts ) ) {
@@ -96,14 +133,24 @@ class WP_REST_Contact_Form_7_Controller extends WP_REST_Controller {
         return true;
     }
 
+
+    /**
+     * @param WP_REST_Request $request
+     * @return mixed|WP_REST_Response
+     * TODO: This should return a JSON representation of the form instead of just the $id
+     */
     public function create_item($request) {
-        $formTemplate = WPCF7_ContactForm::get_template();
-        $form = $this->prepare_item_for_database($request, $formTemplate);
+        $request->set_param("id", -1);
+        $form = $this->prepare_item_for_database($request);
         $id = $form->save();
         $response = rest_ensure_response($id);
         return $response;
     }
 
+    /**
+     * @param WP_REST_Request $request
+     * @return bool|WP_Error
+     */
     public function update_item_permissions_check($request) {
         $post_type = get_post_type_object( $this->post_type );
         if (! current_user_can( $post_type->cap->edit_others_posts ) ) {
@@ -112,6 +159,11 @@ class WP_REST_Contact_Form_7_Controller extends WP_REST_Controller {
         return true;
     }
 
+    /**
+     * @param WP_REST_Request $request
+     * @return mixed|WP_Error|WP_REST_Response
+     * TODO: This should also return a JSON representation of the form instead of just the $id
+     */
     public function update_item($request) {
         $id = (int) $request['id'];
         $form = WPCF7_ContactForm::get_instance( $id );
@@ -120,39 +172,58 @@ class WP_REST_Contact_Form_7_Controller extends WP_REST_Controller {
             return new WP_Error( 'rest_post_invalid_id', __( 'Invalid form id.' ), array( 'status' => 404 ) );
         }
 
-        $form = $this->prepare_item_for_database($request, $form);
+        $request->set_param('id', $id); //TODO: Doubt this is even needed but don't have time to risk it atm
+        $form = $this->prepare_item_for_database($request);
         $id = $form->save();
-        $response = rest_ensure_response($id); //Should I return something else here? Maybe
+        $response = rest_ensure_response($id);
         return $response;
-
     }
 
+    /**
+     * @param mixed $form
+     * @param WP_REST_Request $request
+     * @return mixed|WP_REST_Response
+     */
     public function prepare_item_for_response($form, $request)
     {
         $data = array();
         $data['id'] = $form->id();
-        $data['title'] = $form->title();
         $data['name'] = $form->name();
+        $data['title'] = $form->title();
         $data['locale'] = $form->locale;
         $data['properties'] = $form->get_properties();
         $data = $this->add_additional_fields_to_object($data, $request);
         $response = rest_ensure_response($data);
-        return apply_filters('rest_prepare_wpcf7', $response, $form, $request);
+        $response = apply_filters('rest_api_prepare_wpcf7', $response, $form, $request);
+        return $response;
     }
 
-    protected function prepare_item_for_database($request, $form) {
 
-        $prepared_form = $form;
+    /**
+     * @param WP_REST_Request $request
+     * @return mixed|WPCF7_ContactForm
+     */
+    protected function prepare_item_for_database($request) {
 
-        if ( isset( $request['title'] ) ) {
-            $prepared_form->set_title( $request['title'] );
+        $id  = $request->get_param('id');
+        $prepared_form = WPCF7_ContactForm::get_instance( $id );
+
+        /**
+         * update_item ensures the form ID exists before calling this method to prevent non-existant forms from being created accidentally.
+         */
+        if(empty($prepared_form)) {
+            $prepared_form = WPCF7_ContactForm::get_template();
         }
 
-        if ( isset( $_POST['wpcf7-locale'] ) ) {
-            $locale = trim( $_POST['wpcf7-locale'] );
+        if ( isset( $request['post-title'] ) ) {
+            $prepared_form->set_title( $request['post-title'] );
+        }
+
+        if ( isset( $request['locale'] ) ) {
+            $locale = trim( $request['locale'] );
 
             if ( wpcf7_is_valid_locale( $locale ) ) {
-                $prepared_form->locale = $locale;
+                $prepared_form->set_locale($locale);
             }
         }
 
@@ -240,18 +311,19 @@ class WP_REST_Contact_Form_7_Controller extends WP_REST_Controller {
                 }
             }
 
-            $mail_2['additionalHeaders'] = trim( $headers );
+            $mail_2['additional_headers'] = trim( $headers );
         }
 
         if ( isset( $request['mail-2-attachments'] ) ) {
             $mail_2['attachments'] = trim( $request['mail-2-attachments'] );
         }
 
-        $mail_2['useHtml'] = ! empty( $request['mail-2-use-html'] );
-        $mail_2['excludeBlank'] = ! empty( $request['mail-2-exclude-blank'] );
+        $mail_2['use_html'] = ! empty( $request['mail-2-use-html'] );
+        $mail_2['exclude_blank'] = ! empty( $request['mail-2-exclude-blank'] );
 
         $properties['mail_2'] = $mail_2;
 
+        // For setting/updating confirmation/error messages
         foreach ( wpcf7_messages() as $key => $arr ) {
             $field_name = 'message-' . strtr( $key, '_', '-' );
 
@@ -266,7 +338,8 @@ class WP_REST_Contact_Form_7_Controller extends WP_REST_Controller {
 
         $prepared_form->set_properties( $properties );
 
-        do_action( 'rest_wpcf7_save_contact_form', $prepared_form );
+        //Preceded original with rest_api_
+        do_action( 'rest_api_wpcf7_save_contact_form', $prepared_form );
 
         if ( wpcf7_validate_configuration() ) {
             $config_validator = new WPCF7_ConfigValidator( $prepared_form );
@@ -332,78 +405,78 @@ class WP_REST_Contact_Form_7_Controller extends WP_REST_Controller {
                             'type'        => 'object',
                             'context'     => array( 'embed', 'view', 'edit' ),
                             'properties'  => array(
-                                'subject' => array(
+                                'mail-subject' => array(
                                     'description' => __('Subject of the form'),
                                     'type'      => 'string',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
-                                'sender' => array(
+                                'mail-sender' => array(
                                     'description' => __('Sending email address of the form'),
                                     'type'      => 'string',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
-                                'recipient' => array(
+                                'mail-recipient' => array(
                                     'description' => __('Recipient email address of the form'),
                                     'type'      => 'string',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
-                                'additionalHeaders' => array(
+                                'mail-additional-headers' => array(
                                     'description' => __('Additional form headers'),
                                     'type'      => 'string',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
-                                'attachment' => array(
+                                'mail-attachment' => array(
                                     'description' => __('Attachments to include with form'),
                                     'type'      => 'string',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
-                                'useHtml' => array(
+                                'mail-use-html' => array(
                                     'description' => __('Send emails using HTML formatting'),
                                     'type'      => 'boolean',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
-                                'excludeBlank' => array(
+                                'mail-exclude-blank' => array(
                                     'description' => __('Exclude lines with blank mail-tags from output'),
                                     'type'      => 'boolean',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
                             ),
                         ),
-                        'mail2'  => array(
+                        'mail-2'  => array(
                             'type'        => 'object',
                             'context'     => array( 'embed', 'view', 'edit' ),
                             'properties'  => array(
-                                'subject' => array(
+                                'mail-2-subject' => array(
                                     'description' => __('Subject of the form'),
                                     'type'      => 'string',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
-                                'sender' => array(
+                                'mail-2-sender' => array(
                                     'description' => __('Sending email address of the form'),
                                     'type'      => 'string',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
-                                'recipient' => array(
+                                'mail-2-recipient' => array(
                                     'description' => __('Recipient email address of the form'),
                                     'type'      => 'string',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
-                                'additionalHeaders' => array(
+                                'mail-2-additional-headers' => array(
                                     'description' => __('Additional form headers'),
                                     'type'      => 'string',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
-                                'attachment' => array(
+                                'mail-2-attachment' => array(
                                     'description' => __('Files to attach to the form'),
                                     'type'      => 'string',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
-                                'useHtml' => array(
+                                'mail-2-use-html' => array(
                                     'description' => __('Send emails using HTML formatting'),
                                     'type'      => 'boolean',
                                     'context'   => array( 'embed', 'view', 'edit' ),
                                 ),
-                                'excludeBlank' => array(
+                                'mail-2-exclude-blank' => array(
                                     'description' => __('Exclude lines with blank mail-tags from output'),
                                     'type'      => 'boolean',
                                     'context'   => array( 'embed', 'view', 'edit' ),
